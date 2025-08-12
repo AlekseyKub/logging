3f15d1fad7088fdd943c16defb9574dc96738ca0
Aleksey <aleshakub@gmail.com>
2025-08-12 09:00:19 +0300

    add class

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace AlekseyKub\Logging\Http\Controllers;
 
 abstract class Controller
 {
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace AlekseyKub\Logging\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
